474bd72020181e23d969efef203ccaeaffe7bcff
CMOW5 <tec.cmos@gmail.com>
2018-07-17 21:22:29 -0500

    buildQueryParams method move to baseUrls class

--- src/services/api/urls/products-urls.js
+++ src/services/api/urls/products-urls.js
@@ -69,30 +69,4 @@ export default class ProductsUrls extends BaseUrls {
   static delete(id) {
     return this.base() + `/admin/products/${id}`;
   }
-
-  /**
-   * build the query param string from the params object
-   * @param {object} params
-   * @return {string}
-   */
-  static buildQueryParameters(params) {
-    if (this.isEmpty(params)) return '';
-    let queryParams = '';
-    Object.keys(params).forEach((key) => {
-      queryParams += `${key}=${params[key]}&`;
-    });
-    return `${queryParams}`.replace(new RegExp(' ', 'g'), '%20');
-  }
-
-  /**
-   * check if the given object is empty
-   *
-   * @param {*} object
-   * @return {boolean}
-   */
-  static isEmpty(object) {
-    if (!object) return true;
-    if (Object.keys(object).length > 0) return false;
-    return true;
-  }
 }
