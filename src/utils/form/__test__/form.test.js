bfbbd77a1c1bd1c1e465691b9246adbd6f2e250a
CMOW5 <tec.cmos@gmail.com>
2018-08-13 01:10:54 -0500

    the getformData() function was improved, added test for files and setPut/Patch function

--- src/utils/form/__test__/form.test.js
+++ src/utils/form/__test__/form.test.js
@@ -7,18 +7,10 @@ test('form data is valid', () => {
   });
 
   /* testing the form data */
-  let formData = startState.data();
+  let data = startState.data();
 
-  expect(formData)
+  expect(data)
     .toEqual({'email': 'user@example.com', 'password': '123456'});
-
-  /* testing FormData object */
-  formData = new FormData();
-  formData.set('email', 'user@example.com');
-  formData.set('password', '123456');
-  expect(formData)
-    .toEqual(startState.getFormData());
-
 
   /* testing formData is cleared correctly */
   startState.reset();
@@ -28,8 +20,42 @@ test('form data is valid', () => {
     .toEqual({'email': '', 'password': ''});
 });
 
+test('formData object is valid', () => {
+  const startState = new Form({
+    email: 'user@example.com',
+    password: '123456',
+    metadata: {
+      links: 'some-link',
+    },
+  });
+
+  /* testing FormData object */
+  let formData = new FormData();
+  formData.set('email', 'user@example.com');
+  formData.set('password', '123456');
+  formData.set('metadata', JSON.stringify({links: 'some-link'}));
+
+  expect(formData.get('email'))
+    .toEqual(startState.getFormData().get('email'));
+  expect(formData.get('password'))
+    .toEqual(startState.getFormData().get('password'));
+  expect(formData.get('metadata'))
+    .toEqual(startState.getFormData().get('metadata'));
+
+  /* testing formData is cleared correctly */
+  startState.reset();
+  const resetData = startState.getFormData();
+
+  expect(resetData.get('email'))
+    .toEqual('');
+  expect(resetData.get('password'))
+    .toEqual('');
+  expect(resetData.get('metadata'))
+    .toEqual('');
+});
+
 test('form errors are saved correctly', () => {
-  const startState = new Form({});
+  const form = new Form({});
 
   const errors = {
     'response': {
@@ -48,31 +74,88 @@ test('form errors are saved correctly', () => {
       },
     },
   };
-  /* are errors being saved correctly ? */
-  startState.saveErrors(errors);
 
-  expect(startState.hasError('email'))
+  form.saveErrors(errors);
+
+  expect(form.hasError('email'))
     .toBe(true);
-  expect(startState.hasError('password'))
+  expect(form.hasError('password'))
     .toBe(true);
-  expect(startState.hasError('general'))
+  expect(form.hasError('general'))
     .toBe(true);
 
   /* is error message correct ? */
-  expect(startState.getErrorMessage('email'))
+  expect(form.getErrorMessage('email'))
     .toBe('email error message.');
-  expect(startState.getErrorMessage('password'))
+  expect(form.getErrorMessage('password'))
     .toBe('password error message.');
-  expect(startState.getErrorMessage('general'))
+  expect(form.getErrorMessage('general'))
     .toBe('general error message.');
 
   /* are errors cleared correctly ? */
-  startState.clearErrors();
+  form.clearErrors();
 
-  expect(startState.hasError('email'))
+  expect(form.hasError('email'))
     .toBe(false);
-  expect(startState.hasError('password'))
+  expect(form.hasError('password'))
     .toBe(false);
-  expect(startState.hasError('general'))
+  expect(form.hasError('general'))
     .toBe(false);
 });
+
+test('a single file form is saved correctly in the form', () => {
+  const form = new Form({});
+
+  expect(form.hasFiles())
+    .toBe(false);
+
+  const file = new File([''], 'myFile');
+
+  form.appendFiles('file', file);
+
+  const formData = new FormData();
+  formData.append('file', file);
+
+  expect(form.hasFiles())
+    .toBe(true);
+
+  expect(form.getFormData().get('file'))
+    .toEqual(formData.get('file'));
+});
+
+
+test('an array of files are saved correctly in the form', () => {
+  const form = new Form({});
+
+  expect(form.hasFiles())
+    .toBe(false);
+
+  const files = [new File([''], 'myFile1'), new File([''], 'myFile2')];
+
+  form.appendFiles('files', files);
+
+  const formData = new FormData();
+  formData.append('files[0]', files[0]);
+  formData.append('files[1]', files[1]);
+
+  expect(form.hasFiles())
+    .toBe(true);
+
+  expect(form.getFormData().get('files'))
+    .toEqual(formData.get('files'));
+});
+
+test('the put/patch are correctly set in the form ', () => {
+  const form = new Form({});
+
+  form.setPutMethod();
+
+  expect(form.getFormData().get('_method'))
+    .toEqual('put');
+
+  form.setPatchMethod();
+
+  expect(form.getFormData().get('_method'))
+    .toEqual('patch');
+});
+
